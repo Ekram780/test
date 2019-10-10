200ccdca0bde075df40b8c51cf46ec2a92af9d0c
Ekram780 <54946317+Ekram780@users.noreply.github.com>
2019-10-10 10:42:35 -0400

    final work

---
+++ Script.js
@@ -0,0 +1,16 @@
+function hello(){
+	let username = prompt("What's your name?");
+	if (username == 'ekram'){
+		alert("hello " + username);
+	}
+	else if (username == 'sarah'){
+		alert("give me an A!");
+	}
+	else if (username == 'subi'){
+		alert(username + " is the best");
+	}
+	else{
+		alert("You are not authorized to use this");
+	}
+
+}
