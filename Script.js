767affee3b8df1cbc0bbec96783e883073644848
Ekram780 <54946317+Ekram780@users.noreply.github.com>
2019-10-29 09:41:22 -0400

    fixing

--- Script.js
+++ Script.js
@@ -1,16 +1,40 @@
+// function hello(){
+// 	let guess = prompt("how many sides does a triangle have?");
+// 	if (guess == 1){
+// 		alert("that is wrong");
+// 	}
+// 	else if (guess == 2){
+// 		alert("try again");
+// 	}
+// 	else if (guess ==3){
+// 		alert("correct");
+// 	}
+// 	else{
+// 		alert("keep trying");
+// 	}
+
+// }
+
 function hello(){
-	let username = prompt("What's your name?");
-	if (username == 'ekram'){
-		alert("hello " + username);
+	let guess = prompt("how many sides does a triangle have?");
+	if (guess == 3){
+		alert("correct");
 	}
-	else if (username == 'sarah'){
-		alert("give me an A!");
-	}
-	else if (username == 'subi'){
-		alert(username + " is the best");
-	}
+
 	else{
-		alert("You are not authorized to use this");
+		alert("keep trying");
+	}
+
+}
+
+function question(){
+	let guess = prompt("if I have 120 on one side and 20 on the other what is the remaining side on a trigangle?");
+	if (guess == 40){
+		alert("correct");
+	}
+
+	else{
+		alert("keep trying");
 	}
 
 }
